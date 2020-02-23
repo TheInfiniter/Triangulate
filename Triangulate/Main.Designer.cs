4309321a469aa07cf7f41b63fe6910e8e7b1f96b
Dmitriy <dimonus98@yandex.ru>
2020-02-23 18:46:46 +0300

    first

---
+++ Triangulate/Main.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Triangulate
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcbDrawField = new System.Windows.Forms.PictureBox();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.txtHorizontal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtVertical = new System.Windows.Forms.TextBox();
+            this.chkDebug = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbDrawField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbDrawField
+            // 
+            this.pcbDrawField.BackColor = System.Drawing.Color.White;
+            this.pcbDrawField.Location = new System.Drawing.Point(12, 12);
+            this.pcbDrawField.Name = "pcbDrawField";
+            this.pcbDrawField.Size = new System.Drawing.Size(612, 520);
+            this.pcbDrawField.TabIndex = 0;
+            this.pcbDrawField.TabStop = false;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(672, 64);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(150, 58);
+            this.btnDraw.TabIndex = 1;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // txtHorizontal
+            // 
+            this.txtHorizontal.Location = new System.Drawing.Point(785, 12);
+            this.txtHorizontal.Name = "txtHorizontal";
+            this.txtHorizontal.Size = new System.Drawing.Size(37, 20);
+            this.txtHorizontal.TabIndex = 2;
+            this.txtHorizontal.Text = "5";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(679, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Горизонталь";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(679, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Вертикаль";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtVertical
+            // 
+            this.txtVertical.Location = new System.Drawing.Point(785, 38);
+            this.txtVertical.Name = "txtVertical";
+            this.txtVertical.Size = new System.Drawing.Size(37, 20);
+            this.txtVertical.TabIndex = 4;
+            this.txtVertical.Text = "5";
+            // 
+            // chkDebug
+            // 
+            this.chkDebug.AutoSize = true;
+            this.chkDebug.Location = new System.Drawing.Point(672, 128);
+            this.chkDebug.Name = "chkDebug";
+            this.chkDebug.Size = new System.Drawing.Size(58, 17);
+            this.chkDebug.TabIndex = 6;
+            this.chkDebug.Text = "Debug";
+            this.chkDebug.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 543);
+            this.Controls.Add(this.chkDebug);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtVertical);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHorizontal);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.pcbDrawField);
+            this.Name = "Main";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbDrawField)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbDrawField;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.TextBox txtHorizontal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtVertical;
+        private System.Windows.Forms.CheckBox chkDebug;
+    }
+}
+
---
+++ Triangulate/Main.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Triangulate
+{
+    public struct Points
+    {
+        public Points(double xCoord, double yCoord)
+        {
+            this.xCoord = xCoord;
+            this.yCoord = yCoord;
+        }
+
+        public double xCoord { get; set; }
+        public double yCoord { get; set; }
+    }
+
+    public partial class Main : Form
+    {
+        double leftMargin, rightMargin, topMargin, bottomMargin;
+        int amountVertical, amountHorizontal;
+
+        Graphics graph;
+
+        List<Points> createdPoints;
+
+        public Main()
+        {
+            InitializeComponent();
+            graph = pcbDrawField.CreateGraphics();
+            graph.Clear(Color.White);
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            Points center = new Points(pcbDrawField.Width / 2, pcbDrawField.Height / 2);
+            //graph.TranslateTransform((float)center.xCoord, (float)center.yCoord);
+
+            leftMargin = center.xCoord - 150;
+            rightMargin = center.xCoord + 150;
+            topMargin = center.yCoord - 150;
+            bottomMargin = center.yCoord + 150;
+
+            createdPoints = new List<Points>();
+
+            /*
+            double height = ;
+            double width = 
+            */
+
+            graph.Clear(Color.White);
+
+            Pen pen = new Pen(Color.Black);
+
+            if(chkDebug.Checked)
+            {
+                graph.DrawLine(pen, (float)leftMargin, (float)topMargin, (float)rightMargin, (float)topMargin);
+                graph.DrawLine(pen, (float)rightMargin, (float)topMargin, (float)rightMargin, (float)bottomMargin);
+                graph.DrawLine(pen, (float)rightMargin, (float)bottomMargin, (float)leftMargin, (float)bottomMargin);
+                graph.DrawLine(pen, (float)leftMargin, (float)bottomMargin, (float)leftMargin, (float)topMargin);
+            }
+
+            amountHorizontal = Convert.ToInt32(txtHorizontal.Text);
+            amountVertical = Convert.ToInt32(txtVertical.Text);
+
+            double stepVertical = Math.Abs(topMargin - bottomMargin) / (amountVertical - 1);
+            double stepHorizontal = Math.Abs(rightMargin - leftMargin) / (amountHorizontal - 1);
+
+            double xCurrent = leftMargin, yCurrent = topMargin;
+
+            for (int heightCount = 0; heightCount < amountVertical; heightCount++)
+            {
+                for (int widthCount = 0; widthCount < amountHorizontal; widthCount++)
+                {
+                    createdPoints.Add(new Points(xCurrent, yCurrent));
+                    xCurrent += stepHorizontal;
+                }
+                xCurrent = leftMargin;
+                yCurrent += stepVertical;
+            }
+
+            foreach(Points point in createdPoints)
+            {
+                DrawPoint(point);
+            }
+        }
+
+        private void DrawPoint(Points point)
+        {
+            SolidBrush brush = new SolidBrush(Color.Red);
+
+            graph.FillEllipse(brush, (float)point.xCoord - 2, (float)point.yCoord - 2, 4, 4);
+        }
+    }
+}
